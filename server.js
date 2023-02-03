bc37eb61f60a661b20520eb1376a963d842502ef
Alena-0046 <alena-0046@yandex.ru>
2023-02-03 22:21:33 +0300

    ESLint - add eslint config

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
-const express = require('express');
+const express = require('express')
+const path = require('path')
 
-const app = express();
-const PORT = process.env.PORT || 3000;
+const app = express()
+const PORT = process.env.PORT || 3000
 
-app.use(express.static(__dirname+'/static'));
-app.use(express.static(__dirname+'/dist'));
+app.use(express.static(path.join(__dirname, 'static')))
+app.use(express.static(path.join(__dirname, 'dist')))
 
-app.listen(PORT, function () {
-  console.log(`Example app listening on port ${PORT}!`);
-  console.log(`Directory __dirname = ${__dirname}`);
-}); 
+app.listen(PORT, function() {
+  console.log(`Example app listening on port ${PORT}!`)
+  console.log(`Directory __dirname = ${__dirname}`)
+})
