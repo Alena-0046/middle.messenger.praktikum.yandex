2f2351c47866a3d4aae3e71b56b7db3ecc6b2cc9
Alena-0046 <alena-0046@yandex.ru>
2023-03-11 21:02:04 +0300

    Change links to buttons

--- src/components/leftpanel/leftpanel.ts
+++ src/components/leftpanel/leftpanel.ts
@@ -1,9 +1,20 @@
 import Block from '../../core/block'
+import Link from '../../components/link/link'
 import template from './leftpanel.hbs'
 
 export default class LeftPanel extends Block {
   constructor () {
-    super('div', { attr: { class: 'left-panel' } })
+    const props = {
+      attr: { class: 'left-panel' },
+      link: new Link({
+        attr: {
+          class: 'left-panel__link',
+          textContent: 'Профиль >',
+        },
+        href: '/settings',
+      }),
+    }
+    super('div', props)
   }
 
   render (): DocumentFragment {
