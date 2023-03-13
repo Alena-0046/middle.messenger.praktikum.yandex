620f03fd4cf19039d8c89971ec269b020c5a062a
Alena-0046 <alena-0046@yandex.ru>
2023-03-13 07:49:57 +0300

    Profile - fill inputs with values

--- src/components/input/input.ts
+++ src/components/input/input.ts
@@ -1,7 +1,20 @@
 import Block from '../../core/block'
+import store, { StoreEvents } from '../../core/store'
 
 export default class Input extends Block {
   constructor (props: Record<string, unknown> = {}) {
     super('input', props)
+    store.on(StoreEvents.Updated, () => {
+      const user = store.getState().user
+      if(user != null) {
+        Object.entries(user).forEach(([key, value]) => {
+          if(this.getContent().name === key) {
+            this.setProps({attr: {
+              value: value,
+            }})
+          }
+        })
+      }
+    })
   }
 }
